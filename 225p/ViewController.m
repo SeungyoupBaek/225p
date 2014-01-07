6102ab30311c5575606f766af47f3e48f3f404cb
SeungyoupBaek <bsy8220@naver.com>
2014-01-08 00:41:56 +0900

    225p 완료했습니다.
    225p 완료했습니다.

--- 225p/ViewController.m
+++ 225p/ViewController.m
@@ -8,11 +8,61 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    BOOL moveFlag;
+}
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 
 @end
 
 @implementation ViewController
+
+// 배경을 흰색으로 변경, 이후에 다른 애니메이션 호출
+-(IBAction)series:(id)sender{
+    [UIView beginAnimations:@"series1" context:NULL];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDuration:1.0];
+    // 델리게이트
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(changeBackgroundClear)];
+    
+    self.imageView.backgroundColor = [UIColor whiteColor];
+    
+    [UIView commitAnimations];
+}
+
+-(void)changeBackgroundClear{
+    [UIView beginAnimations:@"series2" context:NULL];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationDelegate:self];
+    
+    self.imageView.backgroundColor = [UIColor clearColor];
+    
+    [UIView commitAnimations];
+}
+
+-(IBAction)move:(id)sender{
+    [UIView beginAnimations:Nil context:NULL];
+    [UIView setAnimationDuration:0.7];
+    
+    if (moveFlag) {
+        self.imageView.center = CGPointMake(self.imageView.center.x + 100, self.imageView.center.y + 100);
+    }else{
+        self.imageView.center = CGPointMake(self.imageView.center.x - 100, self.imageView.center.y - 100);
+    }
+    moveFlag = !moveFlag;
+    
+    [UIView commitAnimations];
+}
+
+// 토글방식으로 투명도 애니메이션
+-(IBAction)alpha:(id)sender{
+    [UIView animateWithDuration:0.7 animations:^{
+        self.imageView.alpha = (self.imageView.alpha == 0.0) ? 1.0 : 0.0;
+    }];
+}
 
 - (void)viewDidLoad
 {
