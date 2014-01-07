c883a6c69e76efd00b17d9a56f3f32ed8cbb6420
SeungyoupBaek <bsy8220@naver.com>
2014-01-07 22:09:00 +0900

    Initial Commit

---
+++ 225p/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  225p
+//
+//  Created by Youp on 2014. 1. 7..
+//  Copyright (c) 2014년 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ 225pTests/_25pTests.m
@@ -0,0 +1,34 @@
+//
+//  _25pTests.m
+//  225pTests
+//
+//  Created by Youp on 2014. 1. 7..
+//  Copyright (c) 2014년 admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _25pTests : XCTestCase
+
+@end
+
+@implementation _25pTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
